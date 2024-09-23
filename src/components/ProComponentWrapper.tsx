360a7c571263fe4d46c8320cfe8e7958df98cf9a
Arif Uddin <arifunctg@gmail.com>
2024-09-23 11:33:41 +0600

    chore: remove unused component

--- src/components/ProComponentWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import withPro from '@utilities/withPro'
-import { type ReactNode } from 'react'
-
-function ProComponentWrapper({ children }: { children: ReactNode }) {
-  return children
-}
-
-export default withPro(ProComponentWrapper, {})
