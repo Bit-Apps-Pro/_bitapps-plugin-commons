3a4fa5b3fa70f25a5a5eabbdb24e0c8cd7f78208
Arif Uddin <arifunctg@gmail.com>
2024-10-02 17:48:48 +0600

    chore: update sync util

--- frontend/bin/sync-plugin-common-files.mjs
+++ frontend/bin/sync-plugin-common-files.mjs
@@ -1,5 +1,7 @@
 import fse from 'fs-extra'
 
-fse.copy('./frontend', '../frontend/_plugin-commons', { overwrite: true })
-fse.copy('./backend/free', '../backend/_plugin-commons', { overwrite: true })
-fse.copy('./backend/pro', '../pro/backend/_plugin-commons', { overwrite: true })
+console.log('Syncing plugin common files...')
+
+fse.copy('./_bitapps-plugin-commons/frontend', './frontend/_plugin-commons', { overwrite: true })
+// fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
+fse.copy('./_bitapps-plugin-commons/backend/pro', './pro/backend/_plugin-commons', { overwrite: true })
