c56768dffb2772fa84e15df9fad317797a398f5c
Arif Uddin <arifunctg@gmail.com>
2024-10-08 16:47:38 +0600

    chore: update plugin sync utils

--- frontend/bin/sync-plugin-common-files.mjs
+++ frontend/bin/sync-plugin-common-files.mjs
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+/* eslint-disable unicorn/prefer-top-level-await */
 
 import fse from 'fs-extra'
 import process from 'node:process'
@@ -9,8 +10,16 @@ if (process.cwd().includes('_bitapps-plugin-commons')) {
   process.chdir('..')
 }
 
-console.log('Current Work Dir', process.cwd())
+console.log('Current Working Dir', process.cwd())
 
-fse.copy('./_bitapps-plugin-commons/frontend', './frontend/_plugin-commons', { overwrite: true })
-// fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
-fse.copy('./_bitapps-plugin-commons/backend/pro', './pro/backend/_plugin-commons', { overwrite: true })
+Promise.all([
+  fse.copy('./_bitapps-plugin-commons/frontend', './frontend/_plugin-commons', { overwrite: true }),
+  // fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
+  fse.copy('./_bitapps-plugin-commons/backend/pro', './pro/backend/_plugin-commons', { overwrite: true })
+])
+  .then(() => {
+    console.log('✅ Plugin common files synced')
+  })
+  .catch(error => {
+    console.error('❌ Error syncing plugin common files', error)
+  })
