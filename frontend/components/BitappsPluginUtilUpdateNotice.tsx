e6ab7f0bd3b57cad93c0458f8c3b4d05a4d8e839
Arif Uddin <arifunctg@gmail.com>
2024-10-14 19:10:09 +0600

    chore: update notice of submodule update

--- frontend/components/BitappsPluginUtilUpdateNotice.tsx
+++ frontend/components/BitappsPluginUtilUpdateNotice.tsx
@@ -3,26 +3,25 @@ import { Alert } from 'antd'
 import Link from 'antd/es/typography/Link'
 
 export function BitAppsPluginUtilUpdateNotice() {
-  if (
-    import.meta.env.DEV &&
-    typeof VITE_PLUGIN_HAS_SUBMODULE_UPDATES !== 'undefined' &&
-    VITE_PLUGIN_HAS_SUBMODULE_UPDATES
-  ) {
+  if (!import.meta.env.DEV) return
+
+  if (typeof VITE_PLUGIN_HAS_SUBMODULE_UPDATES !== 'undefined' && VITE_PLUGIN_HAS_SUBMODULE_UPDATES) {
     return (
       <Alert
         type="warning"
         showIcon
         icon={<LucideIcn name="circle-alert" />}
         closable
-        message={
+        message="Plugin Submodule Update Notice (Dev Only)"
+        css={{ padding: 10 }}
+        description={
           <>
-            "_bitapps-plugin-commons" updated, please pull new changes from{' '}
+            <code>_bitapps-plugin-commons</code> submodule updated, please pull new changes from{' '}
             <Link href="https://github.com/Bit-Apps-Pro/_bitapps-plugin-commons" target="_blank">
               GitHub
             </Link>
-            .
-            <br />
-            If you already pulled the changes, please restart the dev server.
+            . If you already pulled the changes, please restart the dev server. and run{' '}
+            <code>pnpm sync-plugin-commons</code> if needed.
           </>
         }
       />
