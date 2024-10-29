14522279a64795fe1bf2e93001bdee0ad74a29f9
Arif Uddin <arifunctg@gmail.com>
2024-10-29 13:31:53 +0600

    chore: update common sync script

--- frontend/bin/sync-plugin-common-files.mjs
+++ frontend/bin/sync-plugin-common-files.mjs
@@ -12,7 +12,7 @@ if (process.cwd().includes('_bitapps-plugin-commons')) {
 
 console.log('Current Working Dir', process.cwd())
 
-Promise.all([
+await Promise.all([
   fse.emptyDir('./frontend/_plugin-commons'),
   fse.emptyDir('./.config/_plugin-commons'),
   fse.emptyDir('./pro/backend/_plugin-commons')
@@ -20,11 +20,17 @@ Promise.all([
 
 console.log('✅ Delete existing plugin common files')
 
-Promise.all([
+await Promise.all([
+  fse.ensureDir('./frontend/_plugin-commons'),
+  fse.ensureDir('./.config/_plugin-commons'),
+  fse.ensureDir('./pro/backend/_plugin-commons')
+])
+
+await Promise.all([
   fse.copy('./_bitapps-plugin-commons/frontend', './frontend/_plugin-commons', { overwrite: true }),
   fse.copy('./_bitapps-plugin-commons/configs', './.config/_plugin-commons', { overwrite: true }),
-  // fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
   fse.copy('./_bitapps-plugin-commons/backend/pro', './pro/backend/_plugin-commons', { overwrite: true })
+  // fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
 ])
   .then(() => {
     console.log('✅ Plugin common files synced')
