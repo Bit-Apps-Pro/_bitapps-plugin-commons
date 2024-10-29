0597786e5dd87976a0b4b0f9ba1d080416271e9c
Arif Uddin <arifunctg@gmail.com>
2024-10-29 12:14:04 +0600

    chore: update plugin common sync method
    delete dir before merge

--- frontend/bin/sync-plugin-common-files.mjs
+++ frontend/bin/sync-plugin-common-files.mjs
@@ -13,6 +13,14 @@ if (process.cwd().includes('_bitapps-plugin-commons')) {
 console.log('Current Working Dir', process.cwd())
 
 Promise.all([
+  fse.emptyDir('./frontend/_plugin-commons'),
+  fse.emptyDir('./.config/_plugin-commons'),
+  fse.emptyDir('./pro/backend/_plugin-commons')
+])
+
+console.log('✅ Delete existing plugin common files')
+
+Promise.all([
   fse.copy('./_bitapps-plugin-commons/frontend', './frontend/_plugin-commons', { overwrite: true }),
   fse.copy('./_bitapps-plugin-commons/configs', './.config/_plugin-commons', { overwrite: true }),
   // fse.copy('./_bitapps-plugin-commons/backend/free', '/backend/_plugin-commons', { overwrite: true })
