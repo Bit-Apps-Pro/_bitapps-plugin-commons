bcc52e30ab9894d9e227f3b81ab45fb48c43f949
shuvomohajan <shuvomohajan@gmail.com>
2025-01-20 15:47:33 +0600

    fix: update plugin info data to remove translation for titles and descriptions

--- frontend/components/SupportPage/data/pluginInfoData.ts
+++ frontend/components/SupportPage/data/pluginInfoData.ts
@@ -1,3 +1,4 @@
+/* eslint-disable translate-obj-prop/translate-obj-prop */
 import { __ } from '@common/helpers/i18nwrap'
 
 export default {
@@ -5,23 +6,23 @@ export default {
   facebookCommunity: 'https://www.facebook.com/groups/bitcommunityusers',
   linkedIn: 'https://www.linkedin.com/company/bitapps',
   plugins: {
-    'bit-flows': {
+    'bit-pi': {
       buyLink: 'https://bit-flows.com/#pricing/',
-      description: __('Bit Flows - Zapier Alternative in WordPress Unlimited Task.'),
+      description: 'Bit Flows - ' + __('Zapier Alternative in WordPress Unlimited Task.'),
       docLink: 'https://bit-flows.com/documentation/',
       reviewLink: 'https://wordpress.org/support/plugin/bit-pi/reviews/',
-      title: __('Bit Flows'),
+      title: 'Bit Flows',
       website: 'https://bit-flows.com',
       wpSupportThread: 'https://wordpress.org/support/plugin/bit-pi/'
     },
     'bit-social': {
       buyLink: 'https://bit-social.com/#pricing/',
-      description: `An social media management plugin for WordPress, that allows you to share your post to
-      different social media platform, schedule post, manage social media account and many more
-      feature.`,
+      description: __(
+        'An social media management plugin for WordPress, that allows you to share your post to different social media platform, schedule post, manage social media account and many more feature.'
+      ),
       docLink: 'https://bit-social.com/documentation/',
       reviewLink: 'https://wordpress.org/support/plugin/bit-social/reviews/',
-      title: __('Bit Social'),
+      title: 'Bit Social',
       website: 'https://bit-social.com',
       wpSupportThread: 'https://wordpress.org/support/plugin/bit-social/'
     }
